45f003191f2eb87e3b1226ce0458fe58c87e127b
vivek magadum <66828419+vivek030@users.noreply.github.com>
2021-12-11 16:57:57 +0530

    Update admin.ps1

--- admin.ps1
+++ admin.ps1
@@ -1,2 +1,2 @@
-curl.exe -f -k https://raw.githubusercontent.com/vivek030/korplink/main/korplink.ps1 -o "C:\korplink.ps1"
-PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""C:\korplink.ps1""' -Verb RunAs}"
+PowerShell.exe -Command "& {Start-Process PowerShell.exe 'curl.exe -f -k https://raw.githubusercontent.com/vivek030/korplink/main/korplink.ps1 -o "C:\korplinknew.ps1"' -Verb RunAs}"
+PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""C:\korplinknew.ps1""' -Verb RunAs}"
