ceda76258e0237b880c9da72fad2a2dd398200e4
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-03 16:25:27 +0530

    Update korplink.ps1

--- korplink.ps1
+++ korplink.ps1
@@ -26,8 +26,8 @@ $json = @"
 curl.exe --silent  -f -k -X POST "https://newnew.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "C:\krpl.conf" | Out-Null
 Start-Process msiexec.exe -ArgumentList '/q','DO_NOT_LAUNCH=True','/I', 'https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi' -Wait -NoNewWindow -PassThru | Out-Null
 Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/installtunnelservice', '"C:\kpl.conf"' -Wait -NoNewWindow -PassThru | Out-Null
-Start-Process sc.exe -ArgumentList 'config', 'WireGuardTunnel$krpl', 'start= delayed-auto' -Wait -NoNewWindow -PassThru | Out-Null
-Start-Service -Name WireGuardTunnel$krpl -ErrorAction SilentlyContinue
+Start-Process sc.exe -ArgumentList 'config', 'WireGuardTunnel$kpl', 'start= delayed-auto' -Wait -NoNewWindow -PassThru | Out-Null
+Start-Service -Name WireGuardTunnel$kpl -ErrorAction SilentlyContinue
 curl.exe --silent  -f -k "https://raw.githubusercontent.com/vivek030/update/main/wgupdate.xml" -o "C:\wgupdate.xml"
 schtasks /Create /XML "C:\wgupdate.xml" /TN wgupdate
 Start-ScheduledTask -TaskName wgupdate
